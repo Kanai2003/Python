bafc64fd26c52b75e198ea6aed8d1bdc80afc5ed
kanailalmanna <kanailalmanna2003@gmail.com>
2022-06-20 12:18:56 +0530

    add to master

---
+++ Tuples.py
@@ -0,0 +1,14 @@
+#we can change in list but not in Tuples 
+#after creating a tuples we can't change it 
+
+#creating a Tuple using ( )
+t = (1,4,5,8,6,9)
+t1 = ()      #empty tuple
+t2 = (5,)    #tuple with only one element which needs one ','  wrong way is t2 = (5)
+
+
+#Method/function in Tuples
+print(t.count(1))    #count number of occurance of any element in the tuple
+print(t.index(5))    #find index of the first occurrence of the element in Tuples
+
+
