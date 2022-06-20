e1b17787ae57270bf5dc1486c21c750090dfc98e
kanailalmanna <kanailalmanna2003@gmail.com>
2022-06-20 12:19:55 +0530

    add to master

---
+++ Lists.py
@@ -0,0 +1,52 @@
+#creat a list using []
+a = [1,2,3,4,5,]
+print(a)
+
+#access using index
+print(a[0])
+
+#change value using index
+a[0] = 10
+a[1] = 100
+
+print(a)
+
+#we can creat a list with items of different types 
+b = ["Kanai" , 39 , 12 , 'A' , "Gr B "]
+print(b)
+
+
+#list slicing
+friends = ["Sourav" , "Sudip" , "Ritam" , "Adrija" , "Ritwika"]
+print(friends[0:2])              # we can do all the things which we can do in String 
+print(friends[2:5])
+
+
+#Method/function  in list 
+l1 = [1, 3, 15, 9, 5]
+print(l1)
+
+l1.sort()       #sort the list
+print(l1)
+
+l1.reverse()    #reverse the lists
+print(l1)
+
+l1.append(45)    #add a new item to the list
+print(l1)
+
+l1.insert(0,130)    #insert a  new item at any index in the list 
+print(l1)
+
+l1.pop()        #remove the item from the lists using index number
+print(l1)
+
+l1.remove()     #remove the item from the list using value
+print(l1)
+
+
+
+
+
+
+
---
+++ String.py
@@ -0,0 +1,31 @@
+greeting = "Good Morning"
+name = "Kanai"
+
+#concatinating two strings
+c = greeting + " " + name
+print(c)
+
+print(name[3])      #print a character of a strings at the index
+print(name[0:4])    #String slicing or print a sub-String  of any strings 
+print(name[0:])     #is same as name[1:5]
+print(name[:3])     #is same as name[0:3]
+
+#negetive index of string means that index number from back to front 
+print(name[-2])     #is same as name[3]
+print(name[-4:-1])  #is same as name[1:4]
+
+print(name[0:5:2])  #it will print by skipping 2 characters , check it positively
+# here first element is starting index, second element is ending Index and last index is index is skipping number
+
+
+story = "I am Kanai & I want to see my parent's happyness"  #we can use \n for new line \t for tab etc.
+
+#functions for String
+print(len(story))    #print length of story
+print(story.endswith("ness"))     #it will check if String is ending with "ness"
+print(story.count("a"))     #it will count number of any characters or word etc.
+print(story.capitalize())   #it will print capitalized string 
+print(story.find("my"))     #it will check and print index of any characters or word etc.
+print(story.replace("Kanai", "Rit"))    #it will replace Kanai with Rit
+
+
