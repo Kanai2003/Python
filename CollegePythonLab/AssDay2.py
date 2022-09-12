3426f1dfcb1d1c2c4515238f77208883e94e098e
Kanailal Manna <91842029+KanailalManna@users.noreply.github.com>
2022-09-13 01:42:16 +0530

    add to master

--- CollegePythonLab/AssDay2.py
+++ CollegePythonLab/AssDay2.py
@@ -22,13 +22,13 @@ output =
 
 '''
 
-# x={0:0,1:2,2:1,3:4,4:3}
-# l=list(x.items())
-# l.sort()
-# print("Ascending: ",l)
-# t=list(x.items())
-# t.sort(reverse=True)
-# print("Descending: ",t)
+x={0:0,1:2,2:1,3:4,4:3}
+l=list(x.items())
+l.sort()
+print("Ascending: ",l)
+t=list(x.items())
+t.sort(reverse=True)
+print("Descending: ",t)
 
 
 
@@ -41,10 +41,10 @@ input =python
 output=pyhon
 '''
 
-# word = input("Enter your Input : ")
-# index = int(input("Enter index to remove Element: "))
-# output = word[:index - 1] + word[index : ]
-# print(output)
+word = input("Enter your Input : ")
+index = int(input("Enter index to remove Element: "))
+output = word[:index - 1] + word[index : ]
+print(output)
 
 
 
@@ -61,13 +61,13 @@ z={1:’sachin’, 2:’sourav’,3:’rahul’,r:’dhoni’]
 '''
 
 
-# size = int(input("Enter size : "))
-# dic ={}
-# for i in range(size):
-#     key = input("Key: ")
-#     elem = input("Element: ")
-#     dic[key] = elem
-# print(dic)
+size = int(input("Enter size : "))
+dic ={}
+for i in range(size):
+    key = input("Key: ")
+    elem = input("Element: ")
+    dic[key] = elem
+print(dic)
 
 
 
@@ -84,14 +84,14 @@ Write a python program to  search the compartment no and seat no of any passenge
 name as input and also display which compartment is empty
 
 input =
-                x={ 10: {‘a’:11}, 5:{‘b’:21}, {7:{‘c’:9},3: {‘d’:15}, 11:{}  }
+                x={ 10: {‘a’:11}, 5:{‘b’:21}, 7:{‘c’:9},3: {‘d’:15}, 11:{}  }
                 c
 output=
 seat no=9
 compartment no=7
 empty compartment no =11
 input=
-   x={ 10: {‘a’:11}, 5:{‘b’:21}, {7:{‘c’:9},3: {‘d’:15}, 11:{}  }
+   x={ 10: {‘a’:11}, 5:{‘b’:21}, 7:{‘c’:9},3: {‘d’:15}, 11:{}  }
    k
 output=
 passenger name not found
@@ -100,24 +100,24 @@ empty compartment no =11
 
 
 
-# x = {10:{'a':11},5:{'b':21},7:{'c':9},3:{'d':15},11:{} }
+x = {10:{'a':11},5:{'b':21},7:{'c':9},3:{'d':15},11:{} }
 
-# empty =[]
-# bool = 0
-# ch = input("Enter passenger's Name : ")
+empty =[]
+bool = 0
+ch = input("Enter passenger's Name : ")
 
-# for k, dic in x.items():
-#     if len(dic) == 0:
-#         empty.append(k)
-#     elif ch in list(dic.keys()):
-#             print("compartment no: " + str(k))
-#             print("seat no: " + str(dic[ch]))
-#             bool = 1
+for k, dic in x.items():
+    if len(dic) == 0:
+        empty.append(k)
+    elif ch in list(dic.keys()):
+            print("compartment no: " + str(k))
+            print("seat no: " + str(dic[ch]))
+            bool = 1
 
-# if (bool == 0):
-#     print("passenger not found")
+if (bool == 0):
+    print("passenger not found")
     
-# print("empty compartments : " + str(empty))
+print("empty compartments : " + str(empty))
 
 
 
