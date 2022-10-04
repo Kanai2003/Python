c498dbb19fb637c2e37e1f317791aae42121f7a3
UrCodeBuddy12 <114978880+UrCodeBuddy12@users.noreply.github.com>
2022-10-04 12:18:17 +0530

    Update IndianFlag.py
    this a best version of  indian flag using turtle
    
    Signed-off-by: UrCodeBuddy12 <114978880+UrCodeBuddy12@users.noreply.github.com>

--- CodeForFunOnly/IndianFlag.py
+++ CodeForFunOnly/IndianFlag.py
@@ -1,90 +1,101 @@
-import turtle
-from turtle import*
-
-#screen for output
-screen = turtle.Screen()
-
-# Defining a turtle Instance
-t = turtle.Turtle()
-speed(10)
-
-# initially penup()
-t.penup()
-t.goto(-400, 250)
-t.pendown()
-
-# Orange Rectangle
-#white rectangle
-t.color("orange")
-t.begin_fill()
-t.forward(800)
-t.right(90)
-t.forward(167)
-t.right(90)
-t.forward(800)
-t.end_fill()
-t.left(90)
-t.forward(167)
-
-# Green Rectangle
-t.color("green")
-t.begin_fill()
-t.forward(167)
-t.left(90)
-t.forward(800)
-t.left(90)
-t.forward(167)
-t.end_fill()
-
-# Big Blue Circle
-t.penup()
-t.goto(70, 0)
-t.pendown()
-t.color("navy")
-t.begin_fill()
-t.circle(70)
-t.end_fill()
-
-# Big White Circle
-t.penup()
-t.goto(60, 0)
-t.pendown()
-t.color("white")
-t.begin_fill()
-t.circle(60)
-t.end_fill()
-
-# Mini Blue Circles
-t.penup()
-t.goto(-57, -8)
-t.pendown()
-t.color("navy")
-for i in range(24):
-	t.begin_fill()
-	t.circle(3)
-	t.end_fill()
-	t.penup()
-	t.forward(15)
-	t.right(15)
-	t.pendown()
-	
-# Small Blue Circle
-t.penup()
-t.goto(20, 0)
-t.pendown()
-t.begin_fill()
-t.circle(20)
-t.end_fill()
-# Spokes
-t.penup()
-t.goto(0, 0)
-t.pendown()
-t.pensize(2)
-for i in range(24):
-	t.forward(60)
-	t.backward(60)
-	t.left(15)
-	
-#to hold the
-#output window
-turtle.done()
+import turtle  
+from turtle import *  
+  
+# setting the screen for drawing  
+scr = turtle.Screen()  
+# Defining the instance of turtle  
+ttl = turtle.Turtle()  
+speed(20)  
+  
+# keeping the pen up initially  
+ttl.penup()  
+ttl.goto(-150, 125)  
+ttl.pendown(  
+  )  
+# Drawing the Orange Rectangle first  
+#and then the white rectangle  
+ttl.color("orange")  
+ttl.begin_fill()  
+ttl.forward(400)  
+ttl.right(90)  
+ttl.forward(84)  
+ttl.right(90)  
+ttl.forward(400)  
+ttl.end_fill()  
+ttl.left(90)  
+ttl.forward(84)  
+  
+# now drawing the Green Rectangle  
+ttl.color("green")  
+ttl.begin_fill()  
+ttl.forward(84)  
+ttl.left(90)  
+ttl.forward(400)  
+ttl.left(90)  
+ttl.forward(84)  
+ttl.end_fill()  
+  
+# Drawing the central Big Blue Circle  
+ttl.penup()  
+ttl.goto(35, 0)  
+ttl.pendown()  
+ttl.color("navy")  
+ttl.begin_fill()  
+ttl.circle(35)  
+ttl.end_fill()  
+  
+# Drawing the in-circle Big White Circle  
+ttl.penup()  
+ttl.goto(30, 0)  
+ttl.pendown()  
+ttl.color("white")  
+ttl.begin_fill()  
+ttl.circle(30)  
+ttl.end_fill()  
+  
+#Drawing the inside Mini Blue Circles of Flag  
+ttl.penup()  
+ttl.goto(-27, -4)  
+ttl.pendown()  
+ttl.color("navy")  
+for j in range(24):  
+    ttl.begin_fill()  
+    ttl.circle(2)  
+    ttl.end_fill()  
+    ttl.penup()  
+    ttl.forward(7)  
+    ttl.right(15)  
+    ttl.pendown()  
+  
+# drawing the Smaller Blue Circle  
+ttl.color("navy")  
+ttl.penup()  
+ttl.goto(10, 0)  
+ttl.pendown()  
+ttl.begin_fill()  
+ttl.circle(10)  
+ttl.end_fill()  
+  
+#Drawing the 24 spokes of the Indian Flag  
+ttl.penup()  
+ttl.goto(0, 0)  
+ttl.pendown()  
+ttl.pensize(1)  
+for j in range(24):  
+    ttl.forward(30)  
+    ttl.backward(30)  
+    ttl.left(15)  
+  
+#Drawing the stick of the Indian flag  
+ttl.color("Brown")  
+ttl.pensize(10)  
+ttl.penup()  
+ttl.goto(-150,125)  
+ttl.right(180)  
+ttl.pendown()  
+ttl.forward(500)  
+  
+#to hide the turtle pen we used hideturtle  
+ttl.hideturtle()  
+#holding the output on the window  
+turtle.done() 
