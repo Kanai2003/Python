ebaa09c4f2e673ddfb645494c03ddfc9ec64ff3c
Faizan96322 <72128134+Faizan96322@users.noreply.github.com>
2022-10-05 10:27:00 +0530

    Trapping Rain water problem

--- CollegePythonLab/Rain_water_trapped.py
+++ CollegePythonLab/Rain_water_trapped.py
@@ -1,1 +1,17 @@
+def maxArea(A):
+        l = 0
+        r = len(A) -1
+        area = 0
+     
+        while l < r:
+            # Calculating the max area
+            area = max(area, min(A[l],A[r]) * (r - l))
+     
+            if A[l] < A[r]:
+                l += 1
+            else:
+                r -= 1
+        return area
 
+
+        
