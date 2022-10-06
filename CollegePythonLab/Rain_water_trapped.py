f79cc77add8116831240426ac674b5792d56db7a
Kanai <91842029+KanailalManna@users.noreply.github.com>
2022-10-06 18:47:16 +0530

    Update Rain_water_trapped.py
    Signed-off-by: Kanai <91842029+KanailalManna@users.noreply.github.com>

--- CollegePythonLab/Rain_water_trapped.py
+++ CollegePythonLab/Rain_water_trapped.py
@@ -1,1 +1,19 @@
 
+def maxArea(A):
+        l = 0
+        r = len(A) -1
+        area = 0
+
+        while l < r:
+            # Calculating the max area
+            blk=sum(A[l+1:l+r-1])
+            area = max(area, min(A[l],A[r]) * (r - l-1)-blk)
+
+            if A[l] < A[r]:
+                l += 1
+            else:
+                r -= 1
+        
+        return area
+a=[3,0,2,0,4]
+print(maxArea(a))
